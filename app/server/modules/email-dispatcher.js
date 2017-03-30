4cb825ecdf1489b09e81165ae1b4b92488b23372
Jaspreet <js@intersales.de>
2017-03-30 20:15:20 +0200

    chng

--- app/server/modules/email-dispatcher.js
+++ app/server/modules/email-dispatcher.js
@@ -4,8 +4,8 @@ module.exports = EM;
 EM.server = require("emailjs/email").server.connect(
     {
         host 	    : process.env.EMAIL_HOST || 'smtp.gmail.com',
-        user 	    : process.env.EMAIL_USER || 'info.simplewash@gmail.com',
-        password    : process.env.EMAIL_PASS || 'simplewash2016',
+        user 	    : process.env.EMAIL_USER || 'jaashy.singh@gmail.com',
+        password    : process.env.EMAIL_PASS || 'Jass1234$#@!',
         ssl		    : true
     });
 
